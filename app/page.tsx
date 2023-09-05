c368d789dd3913c230f1180513577eb8d08d4229
yisenhuang <1501919885@qq.com>
2023-09-05 10:03:52 +0800

    init初始化

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import SquigglyLines from "../components/SquigglyLines";
 
 export default function HomePage() {
   return (
-    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+    <div className="flex max-w-7xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
         <a
@@ -15,8 +15,8 @@ export default function HomePage() {
           rel="noreferrer"
           className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
         >
-          Clone and deploy your own with{" "}
-          <span className="text-blue-600">Vercel</span>
+          130亿/700亿模型级别 全球TOP1
+          <span className="text-blue-600 mx-2">(130亿 跨级别 超越650亿 meta模型)</span>
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
           Generating dream rooms{" "}
@@ -34,7 +34,7 @@ export default function HomePage() {
           className="bg-blue-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
           href="/dream"
         >
-          Generate your dream room
+          start
         </Link>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,12 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex flex-col xs:flex-row justify-between items-center w-full mt-3 border-b pb-7 sm:px-4 px-2 border-gray-500 gap-2">
+    <header className="flex flex-col xs:flex-row justify-between items-center w-full mt-5 border-b pb-7 sm:px-4 px-2 border-gray-500 gap-2">
       <Link href="/" className="flex space-x-2">
-        <Image
-          alt="header text"
-          src="/bed.svg"
-          className="sm:w-10 sm:h-10 w-9 h-9"
-          width={24}
-          height={24}
-        />
-        <h1 className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">
-          roomGPT.io
+        <h1 className="sm:text-6xl text-xl font-bold ml-2 tracking-tight">X</h1>
+        <h1 className="flex flex-col sm:text-1xl text-xl font-bold ml-2 tracking-tight" >
+          <span className="mx-2">DAN AI</span>
+          <span className="mx-2">新旦智能</span>
         </h1>
       </Link>
       <a
@@ -23,7 +18,7 @@ export default function Header() {
         rel="noopener noreferrer"
       >
         <Github />
-        <p>Star on GitHub</p>
+        <p>Log in</p>
       </a>
     </header>
   );
